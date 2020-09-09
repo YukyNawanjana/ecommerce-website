60434c072d7fe87ceff1bd22f572e940a17ee36c
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-09-09 15:56:29 +0530

    animation add

---
+++ app.js
@@ -0,0 +1,35 @@
+
+//scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin : 'left',
+    duration: 2000,
+    distance:'25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right',{
+    origin : 'right',
+    duration: 2000,
+    distance:'25rem',
+    delay: 600
+});
+
+
+sr.reveal('.animate-top',{
+    origin : 'top',
+    duration: 2000,
+    distance:'25rem',
+    delay: 600
+});
+
+
+sr.reveal('.animate-bottom',{
+    origin : 'bottom',
+    duration: 2000,
+    distance:'25rem',
+    delay: 600
+});
+
